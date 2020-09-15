9455f0dd1d4a05c34f93cab6ef1096ff55a0bb62
Olivier Valentin <ovalentin@osmozis.com>
2020-09-15 12:19:10 +0200

    add simple MII read mode
    command-line flag -m

--- src/oswdiag.c
+++ src/oswdiag.c
@@ -15,7 +15,8 @@ int log_dyn_level = DFLT_LOG_LEVEL;
 static void usage(int argc, char** argv)
 {
     fprintf(stderr,
-            "Usage: %s [-d <dbg_lvl>] [-i <intf>] \n"
+            "Usage: %s [-d <dbg_lvl>] [-i <intf>] { <ath_reg_or_name>  | -m "
+            "<phy> <reg> }\n"
             "\t-d <dbg_lvl>\tset log level. [" AS_STRING(
                 DFLT_LOG_LEVEL) "]\n"
                                 "\t-i <intf>\tset the switch interface "
@@ -23,14 +24,41 @@ static void usage(int argc, char** argv)
             argv[0]);
 }
 
+static int mii_show_content(const char* param_phy, const char* param_reg)
+{
+    uint8_t phy;
+    uint8_t reg;
+    uint16_t val;
+    int res;
+
+    TRACE_ENTER;
+
+    phy = strtoul(param_phy, NULL, 0);
+    reg = strtoul(param_reg, NULL, 0);
+
+    LOG_DEBUG("Reading MII register: <phy=0x%02x> <reg=0x%02x>\n",
+              (unsigned int)phy, (unsigned int)reg);
+
+    res = mdio_read(phy, reg, &val);
+
+    if (res == 0)
+        printf("0x%02x 0x%02x : 0x%04x\n", (unsigned int)phy, (unsigned int)reg,
+               (unsigned int)val);
+
+    TRACE_EXIT;
+
+    return res;
+}
+
 int main(int argc, char** argv)
 {
     int c;
     char* intf_name = "eth0";
+    int mii_mode    = 0;
 
     TRACE_ENTER;
 
-    while ((c = getopt(argc, argv, "d:i:h")) != -1) {
+    while ((c = getopt(argc, argv, "d:i:hm")) != -1) {
         switch (c) {
         case 'd':
             log_dyn_level = atoi(optarg);
@@ -39,6 +67,9 @@ int main(int argc, char** argv)
         case 'i':
             intf_name = optarg;
             LOG_DEBUG("Interface set to %s\n", intf_name);
+            break;
+        case 'm':
+            mii_mode = 1;
             break;
         case 'h':
         case '?':
@@ -52,9 +83,21 @@ int main(int argc, char** argv)
 
     mdio_init(intf_name);
 
-    if (optind < argc)
-        ath_show_content(argv[optind]);
-
+    if (mii_mode) {
+        if (optind < argc + 1) {
+            mii_show_content(argv[optind], argv[optind + 1]);
+        } else {
+            LOG_ERR("required parameters: <phy> <register>\n");
+            usage(argc, argv);
+        }
+    } else {
+        if (optind < argc) {
+            ath_show_content(argv[optind]);
+        } else {
+            LOG_ERR("missing register address or name");
+            usage(argc, argv);
+        }
+    }
     mdio_fini();
 
     TRACE_EXIT;
